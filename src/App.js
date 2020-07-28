7033efdcb9af6d4af4408c625332b23d93e4795f
dreamycodex <dreuphoricme@gmail.com>
2020-07-28 01:29:57 +0100

    completed 1.2 Extract the header in its own component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import Bookings from "./Bookings";
 import "./App.css";
+import Heading from "./Heading";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
       <Bookings />
+      <Heading />
     </div>
   );
 };
