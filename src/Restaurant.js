fea7c9bafc0a7234033ffc79b9913b35604f5fe0
dreamycodex <dreuphoricme@gmail.com>
2020-08-13 00:50:48 +0100

    Completed Task 11. Extract the Add button to its own component

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import RestaurantButton from "./RestaurantButton";
 
-const Restaurant = () => {
+const Restaurant = props => {
   const [orders, setOrders] = useState(0);
 
   function orderOne() {
@@ -11,10 +12,7 @@ const Restaurant = () => {
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {orders}{" "}
-          <button className="btn btn-primary" onClick={orderOne}>
-            Add
-          </button>
+          Pizzas: {orders} <RestaurantButton handleClick={orderOne} />
         </li>
       </ul>
     </div>
