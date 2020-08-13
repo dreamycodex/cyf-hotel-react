a3c7200d672189565a7ddab3fa56fbd13e73fe54
dreamycodex <dreuphoricme@gmail.com>
2020-08-13 23:27:02 +0100

    Completed Task 13. Render more orders

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -6,7 +6,9 @@ const Restaurant = props => {
     <div className="App-content">
       <h3>Restaurant Orders</h3>
       <ul>
-        <Order />
+        <Order orderType={"Pizzas"} />
+        <Order orderType={"Salads"} />
+        <Order orderType={"Chocolate cake"} />
       </ul>
     </div>
   );
